f7e544857cf34169c7dd70f0a590ac43cf3a7f3e
danzel.li <hardanimal@hotmail.com>
2018-01-30 13:22:59 +0800

    Version 1.4.3: Add new test case - self discharge.

--- src/UFT/channel.py
+++ src/UFT/channel.py
@@ -37,6 +37,7 @@ class ChannelStates(object):
     CHECK_TEMP = 0x1C
     DUT_DISCHARGE = 0x1D
     CHECK_POWER_FAIL = 0x1E
+    SELF_DISCHARGE = 0x1F
 
 
 class Channel(threading.Thread):
@@ -341,7 +342,8 @@ class Channel(threading.Thread):
         # start discharge cycle
         all_discharged = False
         start_time = time.time()
-        self.ps.setVolt(0.0)
+        #self.ps.setVolt(0.0)
+        self.ps.deactivateOutput()
         while (not all_discharged):
             all_discharged = True
             for dut in self.dut_list:
@@ -403,7 +405,94 @@ class Channel(threading.Thread):
                             format(dut.slotnum, dut.status, this_cycle.vcap,
                                    this_cycle.temp, dut.errormessage))
             #time.sleep(0)
-        self.ps.setVolt(PS_VOLT)
+        #self.ps.setVolt(PS_VOLT)
+
+    def self_discharge(self):
+        """ self discharge test
+        """
+        for dut in self.dut_list:
+            if dut is None:
+                continue
+            config = load_test_item(self.config_list[dut.slotnum],
+                                    "Self_Discharge")
+            if (not config["enable"]):
+                continue
+            if (config["stoponfail"]) & (dut.status != DUT_STATUS.Idle):
+                continue
+            # disable auto discharge
+            self.switch_to_mb()
+            self.auto_discharge(slot=dut.slotnum, status=False)
+            # disable charge
+            self.switch_to_dut(dut.slotnum)
+            dut.charge(status=False)
+            # enable self discharge
+            dut.self_discharge(status=True)
+
+            dut.status = DUT_STATUS.Discharging
+
+        # start discharge cycle
+        all_discharged = False
+        start_time = time.time()
+        #self.ps.setVolt(0.0)
+        self.ps.deactivateOutput()
+        while (not all_discharged):
+            all_discharged = True
+            for dut in self.dut_list:
+                if dut is None:
+                    continue
+                config = load_test_item(self.config_list[dut.slotnum],
+                                        "Self_Discharge")
+                if (not config["enable"]):
+                    continue
+                if (config["stoponfail"]) & \
+                        (dut.status != DUT_STATUS.Discharging):
+                    continue
+                self.switch_to_dut(dut.slotnum)
+                # cap_in_ltc = dut.meas_capacitor()
+                # print cap_in_ltc
+                this_cycle = Cycle()
+                this_cycle.vin = self.ps.measureVolt()
+                try:
+                    temperature = dut.check_temp()
+                except aardvark.USBI2CAdapterException:
+                    # temp ic not ready
+                    temperature = 0
+                this_cycle.temp = temperature
+                this_cycle.counter = self.counter
+                this_cycle.time = time.time()
+
+                this_cycle.state = "self_discharge"
+                self.ld.select_channel(dut.slotnum)
+                this_cycle.vcap = self.read_volt(dut)
+                # this_cycle.vcap = self.ld.read_volt()
+                self.counter += 1
+
+                threshold = float(config["Threshold"].strip("aAvV"))
+                max_dischargetime = config["max"]
+                min_dischargetime = config["min"]
+
+                discharge_time = this_cycle.time - start_time
+                #dut.discharge_time = discharge_time
+                if (discharge_time > max_dischargetime):
+                    all_discharged &= True
+                    dut.status = DUT_STATUS.Fail
+                    dut.errormessage = "Discharge Time Too Long."
+                elif (this_cycle.vcap < threshold):
+                    all_discharged &= True
+                    if (discharge_time < min_dischargetime):
+                        dut.status = DUT_STATUS.Fail
+                        dut.errormessage = "Discharge Time Too Short."
+                    else:
+                        dut.status = DUT_STATUS.Idle  # pass
+                else:
+                    all_discharged &= False
+                dut.cycles.append(this_cycle)
+                logger.info("dut: {0} status: {1} vcap: {2} "
+                            "temp: {3} message: {4} ".
+                            format(dut.slotnum, dut.status, this_cycle.vcap,
+                                   this_cycle.temp, dut.errormessage))
+            time.sleep(INTERVAL)
+        #self.ps.setVolt(PS_VOLT)
 
     def check_dut_discharge(self):
         """ check auto/self discharge function on each DUT.
@@ -924,7 +1013,15 @@ class Channel(threading.Thread):
                 try:
                     logger.info("Channel: Discharge DUT.")
                     self.discharge_dut()
-                    self.progressbar += 30
+                    self.progressbar += 20
+                    time.sleep(1)
+                except Exception as e:
+                    self.error(e)
+            elif (state == ChannelStates.SELF_DISCHARGE):
+                try:
+                    logger.info("Channel: Discharge DUT.")
+                    self.self_discharge()
+                    self.progressbar += 10
                     time.sleep(1)
                 except Exception as e:
                     self.error(e)
@@ -984,6 +1081,7 @@ class Channel(threading.Thread):
         # self.queue.put(ChannelStates.DUT_DISCHARGE)
         self.queue.put(ChannelStates.LOAD_DISCHARGE)
         self.queue.put(ChannelStates.CHECK_CAPACITANCE)
+        self.queue.put(ChannelStates.SELF_DISCHARGE)
         self.queue.put(ChannelStates.EXIT)
         self.start()
 
